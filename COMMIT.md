443534ad1e909231806d6ab2aed6483d6194815e
ku-baha <kulbarakovbh@gmail.com>
2024-05-22 13:35:45 +0600

    TicTacToe Game write class and abstract methods

--- COMMIT.md
+++ COMMIT.md
@@ -1,11 +1,9 @@
 ### Title
-Solve the tasks problem B. Write test.
+TicTacToe Game write class and abstract methods
 
 ### Summary
-* Problem B
-  * Solve problem b, write method excel_colum
-  * Write test for method excel_colum
+* TicTacToe Game
+  * Write class and abstract methods 
 
 ### Test plan:
-* Run tests. (Instruction in README.md in lesson's directory)
-* Check the correctness of the solution.
+***
--- lesson1/tic_tac_toe_game.py
+++ lesson1/tic_tac_toe_game.py
@@ -1,3 +1,6 @@
+from enum import Enum
+
+
 # This is an open-ended project, but a chance to get some practice coding.
 # The goal is to write a program, in a language of your choice, to let a user play the game tic-tac-toe.
 # The type of UI is up to you: try to not spend too much time on the UI,
@@ -6,3 +9,25 @@
 # Remember to apply the types of principles we’re discussing in this class!
 # Use version control, split up your changes into small commits, write helpful commit messages and test plans.
 # You will be graded on how well you write your code and your messages, not on whether the code works.
+
+class Player(Enum):
+    FIRST = 'X'
+    SECOND = 'O'
+
+
+class TicTacToe:
+    def __init__(self, cols=3, rows=3):
+        self.cols = cols
+        self.rows = rows
+        self.board = [[' ' for _ in range(cols)] for _ in range(rows)]
+        self.current_player = Player.FIRST
+        self.winner = None
+
+    def print_board(self):
+        ...
+
+    def change_player(self):
+        ...
+
+    def set_cell(self, row, col):
+        ...
